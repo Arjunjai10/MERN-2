3944132ad877ebf479320516a4c8556cca5794a8
Arjunjai10 <arjunjaishanmugam.pvn2023Icse@sece.ac.in>
2025-01-20 19:25:21 +0530

    Adding React Component

---
+++ MERN--2/src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./FunctionComponent/About";
+import Constact from "./FunctionComponent/Contact";
+import Gallery from "./FunctionComponent/Gallery";
+import Home from "./FunctionComponent/Home";
+
+
+function App() {
+  return (
+    <section>
+      <About/>
+      <Gallery/>
+      <Home/>
+      <Constact/>
+    </section>
+    
+  );
+}
+
+export default App;
