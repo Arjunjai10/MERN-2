c48004cb71c0dd8e44ea15570470f16fcffbed78
Arjunjai10 <arjunjaishanmugam.pvn2023Icse@sece.ac.in>
2025-01-22 08:43:54 +0530

    DAY-8

--- MERN--2/src/App.jsx
+++ MERN--2/src/App.jsx
@@ -1,18 +1,24 @@
-import About from "./FunctionComponent/About";
-import Constact from "./FunctionComponent/Contact";
-import Gallery from "./FunctionComponent/Gallery";
-import Home from "./FunctionComponent/Home";
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './FunctionComponent/Navbar';
+import Home from './FunctionComponent/Home';
+import About from './FunctionComponent/About';
+import Contact from './FunctionComponent/Contact';
+import Signup from './FunctionComponent/Signup';
+import Gallery from './FunctionComponent/Gallery';
 
 function App() {
   return (
-    <section>
-      <About/>
-      <Gallery/>
-      <Home/>
-      <Constact/>
-    </section>
-    
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ MERN--2/src/FunctionComponent/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../CSS/Navbar.css';
+
+const Navbar = () => {
+    const styling = { 
+        textDecoration: "none", 
+        listStyleType: "none", 
+        textAlign: "center"
+    };
+
+    return (
+        <header>
+            <nav>
+                <ol style={styling}>
+                    <li><Link to="/" className="link">Home</Link></li>
+                    <li><Link to="/about" className="link">About</Link></li>
+                    <li><Link to="/gallery" className="link">Gallery</Link></li>
+                    <li><Link to="/contact" className="link">Contact</Link></li>
+                    <li><Link to="/sign-up" className="link">Signup</Link></li>
+                </ol>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
