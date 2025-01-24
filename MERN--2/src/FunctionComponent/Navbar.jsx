8a8ee6b8fb8e0b8adb213ce1692fe5cc0bbd8b88
Arjunjai10 <arjunjaishanmugam.pvn2023Icse@sece.ac.in>
2025-01-24 09:16:52 +0530

    deploy3

--- MERN--2/src/FunctionComponent/Navbar.jsx
+++ MERN--2/src/FunctionComponent/Navbar.jsx
@@ -22,15 +22,21 @@ const Navbar = () => {
                     <li><Link to="/gallery" className='link'>Gallery</Link></li>
                     <li><Link to="/contact" className='link'>Contact</Link></li>
                     <li><Link to="/sign-up" className='link'>Sign-up</Link></li>
-                    <li><Link to="/login" className='link'>Login</Link></li>   
-                    <li><Link to="/useState" className='link'>useState</Link></li>
-                    <li><Link to="/useeffect" className='link'>useEffect</Link></li> 
+                    <li><Link to="/login" className='link'>Login</Link></li>
+
                     <div>
                     <span onMouseEnter={toggleDrop} onMouseLeave={toggleDrop}>Hooks</span>
                     {dropdown && (
                     <ul>
-                        <li><Link to="/useState" >useState</Link></li>
-                        <li>useEffect</li>
+                        <li><Link to="/useState" >UseState</Link></li>
+                        <li><Link to="/useeffect" >UseEffect</Link></li>
+                        <li><Link to="/usecallback">UseCallback</Link></li>
+                        <li><Link to="/useeffectapi" >UseEffectAPI</Link></li>
+                        <li><Link to="/useeffectimage" >UseEffectAPIimage</Link></li>
+                        <li><Link to="/usereducer" >UseReducer</Link></li>
+                        <li><Link to="/useref" >UseRef</Link></li>
+                        <li><Link to="/usememo" >UseMemo</Link></li>
+                        <li><Link to="/list" >List</Link></li>
                     </ul>)}
                     </div>
                 </ol>
